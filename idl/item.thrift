d2bc82e6de2ea4c920547ac2078818a53ded8e6d
zhaorunshuo <zhaorunshuo@bytedance.com>
2024-12-16 11:23:19 +0800

    feat: 添加代码

---
+++ idl/item.thrift
@@ -0,0 +1,20 @@
+namespace go example.shop.item
+
+include "base.thrift"
+
+struct Item {
+    1: i64 id
+    2: string title
+    3: string description
+    4: i64 stock
+}
+
+struct GetItemRequest {
+    1: required i64 id
+}
+
+struct GetItemResponse {
+    1: Item item
+
+    255: base.BaseResp baseResp
+}
